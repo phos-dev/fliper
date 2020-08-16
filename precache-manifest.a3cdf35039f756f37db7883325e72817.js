d2f9b5182a6c8127435453f64f93781063f1cd9c
Pedro Henrique <38169759+phos-dev@users.noreply.github.com>
2020-08-16 16:48:31 -0300

    Updates

---
+++ precache-manifest.a3cdf35039f756f37db7883325e72817.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b99ec0d3120bdfed1530eb2742bd91cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ef921ab05feec52b8ce",
+    "url": "/static/css/main.86ebb828.chunk.css"
+  },
+  {
+    "revision": "ab0f4e3b13209d79ff7e",
+    "url": "/static/js/2.b3d8ac97.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b3d8ac97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ef921ab05feec52b8ce",
+    "url": "/static/js/main.4889330b.chunk.js"
+  },
+  {
+    "revision": "3803d0e0f21e075b8a09",
+    "url": "/static/js/runtime-main.61c1d36a.js"
+  },
+  {
+    "revision": "28b5e27648f83e7f0cbdfaa663f56a43",
+    "url": "/static/media/android_logo.28b5e276.png"
+  },
+  {
+    "revision": "c8ab11b3736155bc818d789106d46664",
+    "url": "/static/media/asteroids.c8ab11b3.jpg"
+  },
+  {
+    "revision": "5d8711c3eec36cc3f7edcfac5f124501",
+    "url": "/static/media/ios_logo.5d8711c3.png"
+  },
+  {
+    "revision": "abaf530f225870429c648f7f3a910fa4",
+    "url": "/static/media/space invaders.abaf530f.jpg"
+  }
+]);
