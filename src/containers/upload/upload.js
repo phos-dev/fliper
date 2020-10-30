96ef3f485dae37ff4a96ef71e61a066fb58d75d1
Pedro Henrique <38169759+phos-dev@users.noreply.github.com>
2020-10-30 11:56:18 -0300

    Deploy

---
+++ src/containers/upload/upload.js
@@ -0,0 +1,38 @@
+import React from 'react';
+// import styled from 'styled-components';
+// import {useDropzone} from 'react-dropzone';
+
+
+// const Area = styled.div`
+//     height: 200px;
+//     width: 200px;
+//     border-radius: 20px;
+//     background-color: white;
+//     overflow:hidden;
+//     text-align: center;
+// `;
+
+// const App = () => {
+//   return <Unity unityContext={unityContext} />;
+// };
+// const MyDropzone = () => {
+//     const {acceptedFiles, getRootProps, getInputProps} = useDropzone();
+//     const files = acceptedFiles.map(file => (
+//         <li key={file.path}>
+//           {file.path} - {file.size} bytes
+//         </li>
+//       ));
+//     return (
+//         <section className="container">
+//             <div {...getRootProps({className: 'dropzone'})}>
+//                 <input {...getInputProps()} />
+//                 <p>Drag 'n' drop some files here, or click to select files</p>
+//             </div>
+//             <aside>
+//                 <h4>Files</h4>
+//                 <ul>{files}</ul>
+//             </aside>
+//         </section>
+//     );
+// }
+export default App;
